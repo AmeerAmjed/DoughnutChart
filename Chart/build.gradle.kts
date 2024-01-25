f2ca515b73e9880d1fb0bf5d6a1579143db44267
ameeramjed <pr.ameer98@gmail.com>
2024-01-25 13:29:19 +0300

    add chart module

--- Chart/build.gradle.kts
+++ Chart/build.gradle.kts
@@ -1,24 +1,18 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.androidLibrary)
     alias(libs.plugins.kotlinAndroid)
 }
 
 android {
-    namespace = "com.github.ameeramjed"
+    namespace = "com.github.ameeramjed.chart"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.github.ameeramjed"
         minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -43,16 +37,9 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
     }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
 }
 
 dependencies {
-
-    implementation(rootProject.projects.chart)
 
     implementation(libs.core.ktx)
     implementation(libs.lifecycle.runtime.ktx)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,5 +20,7 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "DoughnutChart"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
 include(":app")
- 
+include(":Chart")
